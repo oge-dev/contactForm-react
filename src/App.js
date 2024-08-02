0e38024ead37ca954ecb81d9ca23afa29bc03016
julietoge <julietogechi27@gmail.com>
2024-08-02 17:35:57 +0100

    update

--- src/App.js
+++ src/App.js
@@ -6,22 +6,11 @@ import React from "react";
 function App() {
   const ContactUsApp = (form) => {
     /// Make a POST request using Axios
-    axios
+    return axios
       .post(
         "https://my-json-server.typicode.com/tundeojediran/contacts-api-server/inquiries",
         form
       )
-      .then((response) => {
-        // Log the entire form object for debugging purposes
-        console.log("Response Data:", response.data);
-        
-        // Log specific status from the response
-        console.log("Response Status:", response.status);
-      })
-      .catch((error) => {
-        // Log the entire error for debugging
-        console.error("Error occured:", error);
-      });
   };
 
   return (
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import "./form.css";
 import { nanoid } from "nanoid";
 
-// Generate a unique ID
-const id = nanoid();
-
 const INITIAL_STATE = {
-  id: id,
+  // Generate a unique ID
+  id: nanoid(),
   firstName: "",
   lastName: "",
   email: "",
@@ -16,6 +14,8 @@ const INITIAL_STATE = {
 
 const ContactUsForm = ({ submitForm }) => {
   const [form, setForm] = useState(INITIAL_STATE);
+  const [errors, setErrors] = useState({});
+  const [submitStatus, setSubmitStatus] = useState("");
 
   const handleChange = (e) => {
     setForm({
@@ -36,83 +36,139 @@ const ContactUsForm = ({ submitForm }) => {
     );
   };
 
+  const validateForm = () => {
+    const newErrors = {};
+    if (!form.firstName.trim()) {
+      newErrors.firstName = "First name is required";
+    }
+    if (!form.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(form.email)) {
+      newErrors.email = "Email is invalid";
+    }
+    if (!form.message.trim()) {
+      newErrors.message = "Message is required";
+    }
+    return newErrors;
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Handle form submission
-    submitForm(form);
+    const newErrors = validateForm();
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      setSubmitStatus("");
+    } else {
+      setErrors({});
+      // Handle form submission
+      submitForm(form)
+        .then((response) => {
+          // Log the entire form object for debugging purposes
+        console.log("Response Data:", response.data);
 
-    setForm(INITIAL_STATE);
+        // Log specific status from the response
+        console.log("Response Status:", response.status);
+          setSubmitStatus("Form submitted successfully");
+          setForm(INITIAL_STATE);
+        })
+        .catch((error) => {
+           // Log the entire error for debugging
+          console.error("Error occured:", error);
+          setSubmitStatus(`There was an error submitting the form: ${error.message}`);
+        });
+    }
   };
 
   return (
-      <div className="form-wrapper">
-        <h2>CONTACT US</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group Name-group">
-            <div className="name-input-wrapper">
-              <label htmlFor="firstName">First Name:</label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                value={form.firstName}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="name-input-wrapper">
-              <label htmlFor="lastName">Last Name:</label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                value={form.lastName}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-
-          <div className="form-group email-input-wrapper">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-group subject-input-wrapper">
-            <label htmlFor="subject">Subject:</label>
+    <div className="form-wrapper">
+      <h2>CONTACT US</h2>
+      {submitStatus && (
+        <p
+          className={`submit-status ${
+            submitStatus.includes("error") ? "error-status" : "success"
+          }`}
+        >
+          {submitStatus}
+        </p>
+      )}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group Name-group">
+          <div className="name-input-wrapper">
+            <label htmlFor="firstName">First Name:</label>
             <input
               type="text"
-              id="subject"
-              name="subject"
-              value={form.subject}
+              id="firstName"
+              name="firstName"
+              value={form.firstName}
               onChange={handleChange}
-              required
+            />
+            {errors.firstName && (
+              <p id="firstNameError" className="error">
+                {errors.firstName}
+              </p>
+            )}
+          </div>
+          <div className="name-input-wrapper">
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={form.lastName}
+              onChange={handleChange}
             />
           </div>
-          <div className="form-group message-input-wrapper">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              name="message"
-              cols="30"
-              rows="10"
-              value={form.message}
-              onChange={handleChange}
-              required
-            />
-          </div>
+        </div>
 
-          <div className="btn-wrapper">
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
+        <div className="form-group email-input-wrapper">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            aria-describedby="emailError"
+          />
+          {errors.email && (
+            <p id="emailError" className="error">
+              {errors.email}
+            </p>
+          )}
+        </div>
+        <div className="form-group subject-input-wrapper">
+          <label htmlFor="subject">Subject:</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            value={form.subject}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group message-input-wrapper">
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            cols="30"
+            rows="10"
+            value={form.message}
+            onChange={handleChange} aria-describedby="messageError"
+          />
+          {errors.message && (
+            <p id="messageError" className="error">
+              {errors.message}
+            </p>
+          )}
+        </div>
+
+        <div className="btn-wrapper">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
